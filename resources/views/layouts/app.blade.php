8df536638b2e971a160e8d74818bdb58a43d53f0
= <=>
2020-04-24 16:59:29 +0500

    Configuring database and migrating tables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,21 +50,18 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a href="{{ route('history') }}" class="dropdown-item">{{ __('My orders') }}</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
                             </li>
                         @endguest
-                        <li class="nav-item">
-                            <a href="{{ route('history') }}" class="nav-link">{{ __('My orders') }}</a>
-                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
